292869f6a7b12a0ed08de831cf2ab62221b3abae
elliotmiller18 <egmiller@umich.edu>
2025-04-23 15:55:03 -0400

    removed redundant lines from constructor

--- rom.cpp
+++ rom.cpp
@@ -7,10 +7,6 @@ Rom::~Rom() {}
 
 Rom::Rom(const char* filename) {
     std::ifstream rom(filename, std::ios::binary);
-
-    byte instruction;
-    rom.seekg(0, std::ios::beg);
-
     text.resize(std::filesystem::file_size(filename));
     rom.read(reinterpret_cast<char*>(text.data()), text.size());
 }
