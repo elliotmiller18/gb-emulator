96c954d5085e972570b0175b4042642de7234f1f
elliotmiller18 <egmiller@umich.edu>
2025-06-03 16:18:28 -0700

    major refactors and bug fixes

--- memory.cpp
+++ memory.cpp
@@ -7,34 +7,57 @@ Memory::Memory(Registers& registers) : registers(registers) {}
 
 Memory::~Memory() {}
 
-uint16_t Memory::read(BinOpt16 addr) {
-    uint16_t address = registers.unpack_binopt16(addr);
-    // Note: we don't need to worry about OOB errors as address is a 16 bit integer and memory has 2^16 entries
-    if(address < MEMORY_SIZE - 1) {
-        return (memory[address+1] << 8) | memory[address];
+uint16_t Memory::unpack_addr(BinOpt addr) {
+    if(BinOpt16* unpacked = std::get_if<BinOpt16>(&addr)) {
+        return registers.unpack_binopt16(*unpacked);
     } else {
-        // if we're loading the very last value in memory into a half register
-        return memory[address];
+        return static_cast<uint16_t>(registers.unpack_binopt8(std::get<BinOpt8>(addr))) | EIGHT_BIT_MEM_PREFIX;
     }
 }
 
-uint8_t Memory::read_byte(BinOpt16 addr) {
-    uint16_t address = registers.unpack_binopt16(addr);
+uint16_t Memory::read_word(BinOpt addr) {
+    // TODO: validate implementation
+    uint16_t unpacked_addr = unpack_addr(addr);
+
+    uint16_t upper_byte = memory[unpacked_addr + 1];
+    uint16_t lower_byte = memory[unpacked_addr];
+
+    if(GB_CPU_BIG_ENDIAN) {
+        uint16_t temp = upper_byte;
+        upper_byte = lower_byte;
+        lower_byte = temp;
+    }
+
+    return (upper_byte << 8) | lower_byte;
+}
+
+uint8_t Memory::read_byte(BinOpt addr) {
+    uint16_t address = unpack_addr(addr);
     return memory[address];
 }
 
-bool Memory::write(BinOpt16 addr, BinOpt16 val) {
+bool Memory::write_word(BinOpt addr, BinOpt16 val) {
+    // TODO: validate implementation
     uint16_t unpacked_value = registers.unpack_binopt16(val);
-    uint16_t unpacked_addr = registers.unpack_binopt16(addr);
-    if(unpacked_addr == MEMORY_SIZE) return false;
-    memory[unpacked_value] = static_cast<uint8_t>(unpacked_value);
-    memory[unpacked_value + 1] = static_cast<uint8_t>(unpacked_value >> 8);
+    uint16_t unpacked_addr = unpack_addr(addr);
+
+    uint8_t upper_bits = static_cast<uint8_t>(unpacked_value >> 8);
+    uint8_t lower_bits = static_cast<uint8_t>(unpacked_value);
+
+    if(GB_CPU_BIG_ENDIAN) {
+        uint8_t temp = upper_bits;
+        upper_bits = lower_bits;
+        lower_bits = temp;
+    }
+
+    memory[unpacked_addr + 1] = upper_bits;
+    memory[unpacked_addr] = lower_bits;
     return true;
 }
 
-bool Memory::write_byte(BinOpt16 addr, BinOpt8 val) {
+bool Memory::write_byte(BinOpt addr, BinOpt8 val) {
     uint8_t unpacked_value = registers.unpack_binopt8(val);
-    uint16_t unpacked_addr = registers.unpack_binopt16(addr);
+    uint16_t unpacked_addr = unpack_addr(addr);
     memory[unpacked_addr] = unpacked_value;
     return true;
 }
--- memory.h
+++ memory.h
@@ -5,7 +5,10 @@
 #include "register.h"
 
 constexpr int MEMORY_SIZE = 0xFFFF;
+constexpr int EIGHT_BIT_MEM_PREFIX = 0xFF00;
 constexpr bool BYTE_ADDRESSABLE = true;
+constexpr bool GB_CPU_LITTLE_ENDIAN = true;
+constexpr bool GB_CPU_BIG_ENDIAN = !GB_CPU_LITTLE_ENDIAN;
 
 class Memory
 {
@@ -15,10 +18,11 @@ private:
 public:
     Memory(Registers& registers);
     ~Memory();
-    uint16_t read(BinOpt16 addr);
-    uint8_t read_byte(BinOpt16 addr);
-    bool write(BinOpt16 addr, BinOpt16 val);
-    bool write_byte(BinOpt16 addr, BinOpt8 byte);
+    uint16_t unpack_addr(BinOpt addr);
+    uint16_t read_word(BinOpt addr);
+    uint8_t read_byte(BinOpt addr);
+    bool write_word(BinOpt addr, BinOpt16 val);
+    bool write_byte(BinOpt addr, BinOpt8 byte);
 };
 
 #endif
